74a57414833b4a32f76f1118778a4302c226f420
Anuar Oralov <anuar.oralov@gmail.com>
2023-10-18 01:36:57 +0600

    adding worker

--- app/src/main/java/com/anuar/cryptocurrencyapp/presentation/MyViewModel.kt
+++ app/src/main/java/com/anuar/cryptocurrencyapp/presentation/MyViewModel.kt
@@ -6,9 +6,6 @@ import com.example.cryptoapp.data.repository.CoinRepositoryImpl
 import com.example.cryptoapp.domain.GetCoinInfoListUseCase
 import com.example.cryptoapp.domain.GetCoinInfoUseCase
 import com.example.cryptoapp.domain.LoadDataUseCase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MyViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -20,12 +17,9 @@ class MyViewModel(application: Application) : AndroidViewModel(application) {
 
     val coinInfoList = getCoinInfoListUseCase()
 
-    private val scope= CoroutineScope(Dispatchers.IO)
     fun getDetailInfo(fSym: String) = getCoinInfoUseCase(fSym)
 
     init {
-        scope.launch {
             loadDataUseCase()
-        }
     }
 }
